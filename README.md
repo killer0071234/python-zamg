4c81ed13fe65cfd46756f9d7ee1ed2255a59b4ae
Daniel Gangl <killer007@gmx.at>
2022-11-06 20:49:36 +0000

    Add station parameter to read

--- README.md
+++ README.md
@@ -24,37 +24,56 @@ pip install zamg
 
 ## Usage
 
-Simple usage example to fetch all data from the closest station.
+Simple usage example to fetch specific data from the closest station.
 
 ```python
+"""Asynchronous Python client for ZAMG weather data."""
 import asyncio
 
 import src.zamg.zamg
+from src.zamg.exceptions import ZamgError
+
 
 async def main():
-    """Sample of getting data for the closest station to the given coordinates for the closest station to the given coordinates"""
-    async with src.zamg.zamg.ZamgData() as zamg:
-        data = await zamg.closest_station(46.99, 15.499)
-        zamg.set_default_station(data)
-        print("closest_station = " + str(zamg.get_station_name) + " / " + str(data))
-        await zamg.update()
-        print(
-            "---------- Weather for station %s (%s)",
-            str(zamg.get_data("name", data)),
-            str(data),
-        )
-        for param in zamg.get_all_parameters():
-            print(
-                str(zamg.get_data(parameter=param, data_type="name"))
-                + " -> "
-                + str(zamg.get_data(parameter=param))
-                + " "
-                + str(zamg.get_data(parameter=param, data_type="unit"))
-            )
-        print("last update: %s", zamg.last_update)
+    """Sample of getting data"""
+    try:
+        async with src.zamg.zamg.ZamgData() as zamg:
+            # option to disable verify of ssl check
+            zamg.verify_ssl = False
+            # trying to read zamg station id of the closest station
+            data = await zamg.closest_station(46.99, 15.499)
+            # set closest station as default one to read
+            zamg.set_default_station(data)
+            print("closest_station = " + str(zamg.get_station_name) + " / " + str(data))
+            # print list with all possible parameters
+            print(f"Possible station parameters: {zamg.get_all_parameters()}")
+            # set parameters directly
+            zamg.station_parameters = "TL,SO"
+            # or set parameters as list
+            zamg.set_parameters(("TL", "SO"))
+            # if none of the above parameters are set, all possible parameters are read
+            # do an update
+            await zamg.update()
+
+            print(f"---------- Weather for station {zamg.get_station_name} ({data})")
+            for param in zamg.get_parameters():
+                print(
+                    str(param)
+                    + " -> "
+                    + str(zamg.get_data(parameter=param, data_type="name"))
+                    + " -> "
+                    + str(zamg.get_data(parameter=param))
+                    + " "
+                    + str(zamg.get_data(parameter=param, data_type="unit"))
+                )
+            print("last update: %s", zamg.last_update)
+    except (ZamgError) as exc:
+        print(exc)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 ```
 
 ## Contributions are welcome!
--- src/zamg/zamg.py
+++ src/zamg/zamg.py
@@ -34,9 +34,12 @@ class ZamgData:
     session: aiohttp.client.ClientSession | None = None
     verify_ssl: bool | None = None
     """Set to False to ignore SSL errors."""
+    station_parameters: str | None = None
+    """Comma separated list of station parameter to get from zamg."""
     _timestamp: str | None = None
     _station_id: str = ""
-    _station_parameters: str | None = None
+    _all_station_parameters: str | None = None
+    """Comma separated list of all possible station parameters."""
     _stations: tuple | None = None
 
     def __init__(
@@ -50,7 +53,8 @@ class ZamgData:
         self.session = session
 
     async def zamg_stations(self) -> dict[str:(float, float, str)]:
-        """Return {station_id: (lat, lon, name)} for all public data stations."""
+        """Return {station_id: (lat, lon, name)} for all public data stations.
+        In addition we also get all possible readable parameters for a station."""
 
         def _to_float(val: str) -> str | float:
             try:
@@ -81,7 +85,10 @@ class ZamgData:
                 for parameter in parameter_list:
                     station_parameters += parameter["name"] + ","
                 station_parameters = station_parameters.rstrip(station_parameters[-1])
-                self._station_parameters = station_parameters
+                self._all_station_parameters = station_parameters
+                # also set default station parameter to read
+                if self.station_parameters is None:
+                    self.station_parameters = station_parameters
                 # extract all stations out of parameters
                 station_list = json.loads(contents)["stations"]
                 stations = {}
@@ -130,11 +137,24 @@ class ZamgData:
             raise ZamgNoDataError(exc) from exc
 
     def get_all_parameters(self) -> list[str]:
-        """Get a list of all possible Parameters.
-        The returned list are possible data_type parameter for function get_data()"""
-        if self._station_parameters is None:
+        """Get a list of all possible Parameters which can be read from zamg."""
+        if self._all_station_parameters is None:
             return {}
-        return self._station_parameters.split(",")
+        return self._all_station_parameters.split(",")
+
+    def get_parameters(self) -> list[str]:
+        """Get a list of parameters which are read from zamg.
+        The returned list are possible data_type parameter for function get_data()"""
+        if self.station_parameters is None:
+            return {}
+        return self.station_parameters.split(",")
+
+    def set_parameters(self, param: list[str]) -> None:
+        """Set the list of parameters to read with uodate() function from zamg."""
+        station_parameters = ""
+        for parameter in param:
+            station_parameters += parameter + ","
+        self.station_parameters = station_parameters.rstrip(station_parameters[-1])
 
     @property
     def get_station_name(self) -> str:
@@ -171,7 +191,7 @@ class ZamgData:
             )  # Not time to update yet; we are just reading every 5 minutes
         try:
             # initialize station parameters
-            if self._station_parameters is None:
+            if self.station_parameters is None:
                 await self.zamg_stations()
 
             if self.session is None:
@@ -180,7 +200,7 @@ class ZamgData:
             async with async_timeout.timeout(self.request_timeout):
                 response = await self.session.get(
                     url=self.dataset_data_url
-                    + self._station_parameters
+                    + self.station_parameters
                     + "&station_ids="
                     + str(self._station_id),
                     allow_redirects=False,
