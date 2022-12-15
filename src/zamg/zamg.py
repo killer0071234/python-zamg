0b904fa70236f4bba6531076aa774978953dc512
Tim-Matthias Klecka <tim@klecka.at>
2022-12-15 11:24:41 +0100

    update test data

--- src/zamg/zamg.py
+++ src/zamg/zamg.py
@@ -178,9 +178,9 @@ class ZamgData:
     def last_update(self) -> datetime | None:
         """Return the timestamp of the most recent data."""
         if self._timestamp is not None:
-            return datetime.strptime(
-                self._timestamp, "%Y-%m-%dT%H:%M+00:00"
-            ).replace(tzinfo=zoneinfo.ZoneInfo("Europe/Vienna"))
+            return datetime.strptime(self._timestamp, "%Y-%m-%dT%H:%M+00:00").replace(
+                tzinfo=zoneinfo.ZoneInfo("Europe/Vienna")
+            )
         return None
 
     async def update(self) -> dict | None:
